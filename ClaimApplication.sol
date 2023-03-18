70c49d41cf42a55c4bebbe62f7305ad1f9c5e696
nebosky142 <45599477+OleksiiKoshovyi@users.noreply.github.com>
2023-03-18 13:39:36 +0100

    Change project structure

--- ClaimApplication.sol
+++ ClaimApplication.sol
@@ -12,20 +12,34 @@ contract ClaimApplication {
         bool verified;
         bool paid;
     }
-    
+
     ContractRegistry contractRegistry;
 
     Claim[] private claims;
 
     mapping(address => uint256) indeces;
 
-    event ClaimApplicationSubmitted(address _policyHolder, uint256 _policyNumber);
-    event ClaimApplicationVerified(address _policyHolder, uint256 _policyNumber);
-    event ClaimApplicationApproved(address _policyHolder, uint256 _policyNumber);
-    event ClaimApplicationRejected(address _policyHolder, uint256 _policyNumber);
+    event ClaimApplicationSubmitted(
+        address _policyHolder,
+        uint256 _policyNumber
+    );
+    event ClaimApplicationVerified(
+        address _policyHolder,
+        uint256 _policyNumber
+    );
+    event ClaimApplicationApproved(
+        address _policyHolder,
+        uint256 _policyNumber
+    );
+    event ClaimApplicationRejected(
+        address _policyHolder,
+        uint256 _policyNumber
+    );
 
     modifier onlyInsurer() {
-        Policy policyContract = Policy(payable(contractRegistry.getContract(REGISTRY_KEY_POLICY)));
+        Policy policyContract = Policy(
+            payable(contractRegistry.getContract(REGISTRY_KEY_POLICY))
+        );
         address payable insurer = payable(policyContract.insurer());
         require(msg.sender == insurer, "Only the insurer can verify claims");
         _;
@@ -33,7 +47,10 @@ contract ClaimApplication {
 
     modifier hasClaimApplication(address _policyHolder) {
         uint256 _claimIndex = indeces[_policyHolder];
-        require(_claimIndex != 0, "Policy holder doesn't have any claim applications");
+        require(
+            _claimIndex != 0,
+            "Policy holder doesn't have any claim applications"
+        );
         _;
     }
 
@@ -42,19 +59,36 @@ contract ClaimApplication {
     }
 
     function submitClaim(uint256 _ClaimAmount) external payable {
-        Policy policyContract = Policy(payable(contractRegistry.getContract(REGISTRY_KEY_POLICY)));
-        Policy.PolicyDetails memory _policyDetails = policyContract.getPolicyDetails(msg.sender);
-        require(msg.value == _policyDetails.deductible,
-            "Policyholder should pay deductible for applying");
+        Policy policyContract = Policy(
+            payable(contractRegistry.getContract(REGISTRY_KEY_POLICY))
+        );
+        Policy.PolicyDetails memory _policyDetails = policyContract
+            .getPolicyDetails(msg.sender);
+        require(
+            msg.value == _policyDetails.deductible,
+            "Policyholder should pay deductible for applying"
+        );
         require(_ClaimAmount > 0, "Claim amount must be greater than zero");
-        require(policyContract.isPolicyHolder(msg.sender),
-            "Only policy holders can submit claims");
-        require(policyContract.isPolicyStarted(msg.sender),
-            "Policy isn't activated");
-        require(policyContract.isPolicyDeadlineValid(msg.sender),
-            "Pyment deadline has been missed. The policy is inactive");
-        require(policyContract.isPolicyEndTimeValid(msg.sender), "Policy expired");
-        require(indeces[msg.sender] == 0, "Policy holder has already applied for claim");
+        require(
+            policyContract.isPolicyHolder(msg.sender),
+            "Only policy holders can submit claims"
+        );
+        require(
+            policyContract.isPolicyStarted(msg.sender),
+            "Policy isn't activated"
+        );
+        require(
+            policyContract.isPolicyDeadlineValid(msg.sender),
+            "Pyment deadline has been missed. The policy is inactive"
+        );
+        require(
+            policyContract.isPolicyEndTimeValid(msg.sender),
+            "Policy expired"
+        );
+        require(
+            indeces[msg.sender] == 0,
+            "Policy holder has already applied for claim"
+        );
 
         // Create a new claim and add it to the list
         Claim memory newClaim = Claim({
@@ -72,11 +106,12 @@ contract ClaimApplication {
         emit ClaimApplicationSubmitted(msg.sender, _policyDetails.policyNumber);
     }
 
-    function verifyClaim(address _policyHolder)
-        external
-        hasClaimApplication(_policyHolder)
-        onlyInsurer {
-        Policy policyContract = Policy(payable(contractRegistry.getContract(REGISTRY_KEY_POLICY)));
+    function verifyClaim(
+        address _policyHolder
+    ) external hasClaimApplication(_policyHolder) onlyInsurer {
+        Policy policyContract = Policy(
+            payable(contractRegistry.getContract(REGISTRY_KEY_POLICY))
+        );
         // Verify the claim and mark it as verified
         uint256 _claimIndex = indeces[_policyHolder] - 1;
         claims[_claimIndex].verified = true;
@@ -85,25 +120,34 @@ contract ClaimApplication {
         emit ClaimApplicationVerified(_policyHolder, _policyNumber);
     }
 
-    function rejectClaim(address _policyHolder)
-        external
-        hasClaimApplication(_policyHolder)
-        onlyInsurer {
-        Policy policyContract = Policy(payable(contractRegistry.getContract(REGISTRY_KEY_POLICY)));
+    function rejectClaim(
+        address _policyHolder
+    ) external hasClaimApplication(_policyHolder) onlyInsurer {
+        Policy policyContract = Policy(
+            payable(contractRegistry.getContract(REGISTRY_KEY_POLICY))
+        );
         // Remove the claim from the list
         _removeItem(_policyHolder);
-        
+
         uint256 _policyNumber = policyContract.getPolicyNumber(_policyHolder);
         emit ClaimApplicationRejected(_policyHolder, _policyNumber);
     }
 
-    function payClaim(address _policyHolder) external onlyInsurer() {
-        Policy policyContract = Policy(payable(contractRegistry.getContract(REGISTRY_KEY_POLICY)));
+    function payClaim(address _policyHolder) external onlyInsurer {
+        Policy policyContract = Policy(
+            payable(contractRegistry.getContract(REGISTRY_KEY_POLICY))
+        );
         uint256 _policyNumber = policyContract.getPolicyNumber(_policyHolder);
         // Check that the claim has been verified and is not already paid
         uint256 _claimIndex = indeces[_policyHolder] - 1;
-        require(claims[_claimIndex].verified == true, "Claim has not been verified");
-        require(claims[_claimIndex].paid == false, "Claim has already been paid");
+        require(
+            claims[_claimIndex].verified == true,
+            "Claim has not been verified"
+        );
+        require(
+            claims[_claimIndex].paid == false,
+            "Claim has already been paid"
+        );
 
         // Mark the claim as paid
         claims[_claimIndex].paid = true;
@@ -116,17 +160,15 @@ contract ClaimApplication {
         emit ClaimApplicationApproved(_policyHolder, _policyNumber);
     }
 
-    function getClaim(address _policyHolder)
-        public
-        view
-        hasClaimApplication(_policyHolder)
-        returns(Claim memory) {
+    function getClaim(
+        address _policyHolder
+    ) public view hasClaimApplication(_policyHolder) returns (Claim memory) {
         return claims[indeces[_policyHolder] - 1];
     }
 
-    function _removeItem(address _policyHolder)
-        internal
-        hasClaimApplication(_policyHolder) {
+    function _removeItem(
+        address _policyHolder
+    ) internal hasClaimApplication(_policyHolder) {
         uint256 _claimIndex = indeces[_policyHolder];
 
         indeces[_policyHolder] = 0;
